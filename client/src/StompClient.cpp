298e00fdb6c40cbac337f0a0c5a939bdd0e3ea41
roru0200 <rubinrotem@gmail.com>
2026-01-20 18:38:20 +0000

    client main program implementation

--- client/src/StompClient.cpp
+++ client/src/StompClient.cpp
@@ -1,65 +1,102 @@
 #include <stdlib.h>
 #include "../include/ConnectionHandler.h"
-void SocketReader(ConnectionHandler* connectionHandler){
-	// Implementation of SocketReader that reads from the socket
-	// and processes incoming messages until shouldTerminate is true.
+#include "../include/StompProtocol.h"
+using std::unique_ptr;
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::stringstream;
+using std::vector;
+using std::thread;
 
+void SocketReaderThread(ConnectionHandler* connectionHandler, StompProtocol& protocol) {
+	while (protocol.isLoggedIn()) {
+		string response;
+		if (!connectionHandler->getFrameAscii(response, '\0')) {
+			cout << "Disconnected from server, exiting..." << endl;
+			protocol.setLoggedIn(false);
+			break;
+		}
+		protocol.processMessage(response);
+	}
 }
 
 int main(int argc, char *argv[]) {
-	ConnectionHandler* handler = nullptr;
-	StompProtocol* protocol = nullptr;
-	bool loggedIn = false;
-	while (1) {
-		const short bufsize = 1024;
-		char buf[bufsize];
-		std::cin.getline(buf, bufsize);
-		std::string line(buf);
-		std::stringstream line_stream(line);
-        std::string command;
-        line_stream >> command;
-		if (command == "login") {
-			if (loggedIn) {
-				std::cout << "the client is already logged in, log out before trying again" << std::endl;
+	while (1){
+		unique_ptr<ConnectionHandler> handler;
+		StompProtocol protocol;
+
+		//=============================LOGIN LOOP START================================
+		while (!protocol.isLoggedIn()) {
+			// get user input
+			const short bufsize = 1024;
+			char buf[bufsize];
+			cin.getline(buf, bufsize);
+			string line(buf);
+
+			// parse user input
+			stringstream line_stream(line);
+			string command;
+			line_stream >> command;
+
+			if (command == "login") {
+				//setting up connection handler if first login attempt
+				string host_port;
+				line_stream >> host_port;
+				size_t colon = host_port.find(':');
+				std::string host = host_port.substr(0, colon);
+				short port = atoi(host_port.substr(colon + 1).c_str());
+
+				handler.reset(new ConnectionHandler(host, port));
+
+				if(!handler->connect()){
+					cout << "Could not connect to server" << endl;
+					continue;
+				}
+				// socket connected, send login frame
+				vector<string> login_frame_vector = protocol.proccessKeyboardMessage(line);
+				string login_frame = login_frame_vector[0];
+				handler->sendFrameAscii(login_frame, '\0');
+				//proccessing server response
+				string response;
+				handler->getFrameAscii(response, '\0');
+				protocol.processServerMessage(response);
+
 				continue;
 			}
-			std::string hostPort;
-            line_stream >> hostPort;
-            
-            size_t colon = hostPort.find(':');
-
-			std::string host = hostPort.substr(0, colon);
-			short port = atoi(hostPort.substr(colon + 1).c_str());
-
-            handler = new ConnectionHandler(host, port);
-            if (!handler->connect()) {
-                std::cout << "Could not connect to server" << std::endl;
-				delete handler;
-                handler = nullptr;
-                continue;
-			}
-			std::string username;
-			std::string password;
-			line_stream >> username;
-			line_stream >> password;
-			protocol = new StompProtocol(username, handler);
-			protocol->sendLogin(username, password);
-			std::string answer;
-			handler->getFrameAscii(answer, '\0')
-			protocol.processMessage(answer);
-
-
-			
-		} else if (command == "join") {
-			// handle join
-		} else if (command == "report") {
-			// handle report
-		} else if (command == "summery") {
-			// handle summery
-		} else if (command == "logout") {
-			// handle logout
-		} else {
-			std::cout << "Unknown command" << std::endl;
+			// if command is not login
+			cout << "You must log in first" << endl;
 		}
+		//=============================LOGIN LOOP END=================================
+
+		//login was successful, start socket thread
+		std::thread socketThread(SocketReaderThread, handler.get(), std::ref(protocol));	
+
+		//=============================LOGGED IN LOOP START===========================
+		while (protocol.isLoggedIn()) {
+			const short bufsize = 1024;
+			char buf[bufsize];
+			cin.getline(buf, bufsize);
+			string line(buf);
+
+			if (!protocol.isLoggedIn()) break;
+
+			if (!line.empty()){
+				vector<string> frames_vector = protocol.proccessKeyboardMessage(line);
+				for (const string& frame : frames_vector) 
+					handler->sendFrameAscii(frame, '\0');
+			}
+
+			//gracfully handle logout, to not hang waiting for keyboard input 
+			if (line.substr(0, 6) == "logout") {
+                    if (socketThread.joinable()) socketThread.join();
+                    break;
+                }
+            }
+			//=============================LOGGED IN LOOP END===========================
+			
+		    //in case of sudden exit caused by an error message, clean up
+			if (socketThread.joinable()) socketThread.join();
+		}	
 	}
-}
